a053b5fd4d7f6e5ed9014e708eb84f795f1167e2
yyc <34782655@qq.com>
2020-05-13 14:02:36 +0800

    删除多余代码

--- src/main/java/com/yyc/demo/config/ResponseControllerAdvice.java
+++ src/main/java/com/yyc/demo/config/ResponseControllerAdvice.java
@@ -35,8 +35,8 @@ public class ResponseControllerAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public Object beforeBodyWrite(Object data, MethodParameter returnType, MediaType mediaType,
                                   Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest request, ServerHttpResponse response) {
-        // String类型不能直接包装，所以要进行些特别的处理，另一种处理方法可以使用WebMvcConfigurationSupport，参见./WebConfig.java
-        // FIXME 不进行处理为啥会报类型转换异常：ResponseVO不能转为String？https://stackoverflow.com/questions/51828879/springboot-controller-return-data-through-responsebodyadvice-occur-an-error-cl/52200198#52200198?newreg=c566fe47bbf94e6ea14d503011797c5a
+        // String类型不能直接包装，所以要进行些特别的处理，另一种处理方法可以使用WebMvcConfigurationSupport，参见README.md中的链接3
+        // 不进行处理为啥会报类型转换异常：ResponseVO不能转为String？参考README.md中的链接2
         if (returnType.getGenericParameterType().equals(String.class)) {
             ObjectMapper objectMapper = new ObjectMapper();
             try {
